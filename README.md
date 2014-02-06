42cc2937390574b163063e6af937d14b8decfa8a
moznion <moznion@gmail.com>
2014-02-06 19:08:54 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebService::ImKayac::Simple - It's new $module
+
+# SYNOPSIS
+
+    use WebService::ImKayac::Simple;
+
+# DESCRIPTION
+
+WebService::ImKayac::Simple is ...
+
+# LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+moznion <moznion@gmail.com>
---
+++ lib/WebService/ImKayac/Simple.pm
@@ -0,0 +1,39 @@
+package WebService::ImKayac::Simple;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::ImKayac::Simple - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::ImKayac::Simple;
+
+=head1 DESCRIPTION
+
+WebService::ImKayac::Simple is ...
+
+=head1 LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+moznion E<lt>moznion@gmail.comE<gt>
+
+=cut
+
